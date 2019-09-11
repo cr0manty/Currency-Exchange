23d459608328a3a18d5a86e64e6fece4760d0ac0
Cr0manty <aappland@gmail.com>
2019-09-11 03:45:35 +0300

    heroku db fix

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
+import os
 token = '973630042:AAE3jwuO6d7H9enrFbRRmuQlQkkun9wAdD4'
 course_api = 'https://api.cryptonator.com/api/ticker/{}-{}'
 web_hook_url = 'https://money-exchange-currency-bot.herokuapp.com/'
@@ -6,4 +7,4 @@ web_hook_url = 'https://money-exchange-currency-bot.herokuapp.com/'
 class Configuration(object):
     DEBUG = True
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///curenncy.db'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///curenncy.db'
